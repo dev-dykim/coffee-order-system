e441b186a8e3168e897fedfcf80bbc3d4797600f
doyeonKim <0915dy@gmail.com>
2023-05-20 00:41:06 +0900

    Feat: #30 인기 메뉴 조회시 캐시 적용
    - Redis Cache Config 설정
    - Application에 `@EnableCaching` 어노테이션 추가
    - 1시간마다 cache 비우도록 설정
    
    closes #30

--- src/main/java/com/example/coffee/menu/service/MenuService.java
+++ src/main/java/com/example/coffee/menu/service/MenuService.java
@@ -4,6 +4,9 @@ import com.example.coffee.menu.dto.MenuResponseDto;
 import com.example.coffee.menu.dto.PopularMenuResponseDto;
 import com.example.coffee.menu.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,14 +19,18 @@ public class MenuService {
 
 
     public List<MenuResponseDto> getAllMenus() {
-
         return menuRepository.findAll().stream()
                 .map(MenuResponseDto::from)
                 .toList();
     }
 
+    @Cacheable(cacheNames = "popular-menus")
     public List<PopularMenuResponseDto> getPopularMenus() {
-
         return menuRepository.findWeeklyTopThreeMenus();
+    }
+
+    @Scheduled(cron = "0 0 * * * *")
+    @CacheEvict(cacheNames = "popular-menus", allEntries = true)
+    public void clearPopularMenusCache() {
     }
 }
